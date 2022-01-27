366f553b4a53fb8011a04302d62a8b9dc0d7ed01
Romain <96626929+Romanitho@users.noreply.github.com>
2022-01-27 21:59:33 +0100

    added -Silent

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Start-Process 'powershell.exe' -Argument '-executionpolicy bypass -file """%~dp0winget-install-and-update.ps1""'" -Verb RunAs
+powershell -Command "Start-Process 'powershell.exe' -Argument '-executionpolicy bypass -file """%~dp0winget-install-and-update.ps1"" -Silent'" -Verb RunAs

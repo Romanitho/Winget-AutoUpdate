2557afa06f1361bb2ed04a5c34541bc1ab25e3a8
Adam Niedzwiedz <adam@bearit.com.au>
2022-02-20 15:17:55 +0800

    add my mods

---
+++ Winget-AutoUpdate-Customisations.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+Update Winget-AutoUpdate with customisations.
+
+.DESCRIPTION
+Run the scheduled task only when idle and only once per day
+https://github.com/AdamBearWA/Winget-AutoUpdate
+
+.EXAMPLE
+.\Winget-AutoUpdate-Customisations.ps1
+#>
+
+[CmdletBinding()]
+param(
+)
+
+<# FUNCTIONS #>
+
+function Update-WingetAutoUpdate{
+    try{
+        #Check if previous version location exists and delete
+        Get-ScheduledTask -TaskName "Winget Update" -ErrorAction Stop -OutVariable $task
+
+        if ($task) {
+            # Settings for the scheduled task for Updates
+            $taskTrigger = New-ScheduledTaskTrigger -Daily -At 6AM
+            $taskSettings = New-ScheduledTaskSettingsSet -StartWhenAvailable -DontStopIfGoingOnBatteries -ExecutionTimeLimit 03:00:00 -RunOnlyIfIdle
+
+            # Update the scheduled task
+            Set-ScheduledTask -TaskName 'Winget-AutoUpdate' -Settings $taskSettings -Trigger $taskTrigger
+
+            Write-host "`nInstallation succeeded!" -ForegroundColor Green
+        } else {
+            Write-host "`nInstallation failed! Make sure Winget-AutoUpdate has installed successfully before running this script." -ForegroundColor Green
+            return $False
+        }
+
+        Start-sleep 1
+    }
+    catch{
+        Write-host "`nInstallation failed! Run me with admin rights." -ForegroundColor Red
+        Start-sleep 1
+        return $False
+    }
+}
+
+<# MAIN #>
+
+Write-host "###################################"
+Write-host "#                                 #"
+Write-host "#        Winget AutoUpdate        #"
+Write-host "#          Customisations         #"
+Write-host "#                                 #"
+Write-host "###################################`n"
+Write-host "Applying customisations..."
+
+Install-WingetAutoUpdate
+
+Start-Sleep 3
--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 @echo off
 powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" -Silent'" -Verb RunAs
+powershell -Command "Start-Process powershell.exe -Argument '-executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Customisations.ps1"" -Silent'" -Verb RunAs

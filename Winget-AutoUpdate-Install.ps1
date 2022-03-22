a57f7d838aefa469b9a94d8d8be68576b7376035
Romain <96626929+Romanitho@users.noreply.github.com>
2022-03-22 08:29:19 +0100

    Update Winget-AutoUpdate-Install.ps1

--- Winget-AutoUpdate-Install.ps1
+++ Winget-AutoUpdate-Install.ps1
@@ -102,7 +102,7 @@ function Install-WingetAutoUpdate{
         & reg add "HKCR\AppUserModelId\Windows.SystemToast.Winget.Notification" /v IconUri /t REG_EXPAND_SZ /d %SystemRoot%\system32\@WindowsUpdateToastIcon.png /f | Out-Null
 
         # Settings for the scheduled task for Updates
-        $taskAction = New-ScheduledTaskAction –Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$($WingetUpdatePath)\winget-upgrade.ps1`""
+        $taskAction = New-ScheduledTaskAction –Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$($WingetUpdatePath)\winget-upgrade.ps1`""
         $taskTrigger1 = New-ScheduledTaskTrigger -AtLogOn
         $taskTrigger2 = New-ScheduledTaskTrigger  -Daily -At 6AM
         $taskUserPrincipal = New-ScheduledTaskPrincipal -UserId S-1-5-18 -RunLevel Highest
@@ -113,7 +113,7 @@ function Install-WingetAutoUpdate{
         Register-ScheduledTask -TaskName 'Winget-AutoUpdate' -InputObject $task -Force
 
         # Settings for the scheduled task for Notifications
-        $taskAction = New-ScheduledTaskAction –Execute "wscript.exe" -Argument "`"$($WingetUpdatePath)\Invisible.vbs`" `"powershell.exe -ExecutionPolicy Bypass -File `"`"`"$($WingetUpdatePath)\winget-notify.ps1`"`""
+        $taskAction = New-ScheduledTaskAction –Execute "wscript.exe" -Argument "`"$($WingetUpdatePath)\Invisible.vbs`" `"powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"`"`"$($WingetUpdatePath)\winget-notify.ps1`"`""
         $taskUserPrincipal = New-ScheduledTaskPrincipal -GroupId S-1-5-11
         $taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8 -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 00:05:00
 

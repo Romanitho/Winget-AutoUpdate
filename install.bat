ccf988bb3cf88d03c016b21ad1b9b3008457f842
Romain <96626929+Romanitho@users.noreply.github.com>
2022-04-05 15:19:58 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" -UseWhiteList -Silent'" -Verb RunAs
+powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" -Silent'" -Verb RunAs

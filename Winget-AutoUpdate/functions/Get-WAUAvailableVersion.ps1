b4866a77b56b0a43755d8a72bca2a4004ee3e559
Romain <96626929+Romanitho@users.noreply.github.com>
2022-05-25 12:26:29 +0200

    Added Log for Pre-release version if enabled

--- Winget-AutoUpdate/functions/Get-WAUAvailableVersion.ps1
+++ Winget-AutoUpdate/functions/Get-WAUAvailableVersion.ps1
@@ -5,6 +5,9 @@ function Get-WAUAvailableVersion {
     #Get Github latest version
     if ($WAUConfig.WAU_UpdatePrerelease -eq 1) {
         
+        #Log
+        Write-log "WAU AutoUpdate Pre-release versions is Enabled" "Cyan"
+        
         #Get latest pre-release info
         $WAUurl = 'https://api.github.com/repos/Romanitho/Winget-AutoUpdate/releases'
     

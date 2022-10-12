ba671161d8d868a92a73ea288a33ac6d583eb34b
Romain <96626929+Romanitho@users.noreply.github.com>
2022-10-12 22:07:47 +0200

    Set ACL if log file exists

--- install.bat
+++ install.bat
@@ -1,2 +1,5 @@
 @echo off
-powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" -UpdatesAtLogon'" -Verb RunAs
+
+SET arguments=-UpdatesAtLogon -UpdatesInterval Weekly -StartMenuShortcut
+
+powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" %arguments%'" -Verb RunAs

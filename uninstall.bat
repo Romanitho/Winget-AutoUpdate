0b1389e8aacec4d84f9101d998cf8202f9afb7cb
Romain <96626929+Romanitho@users.noreply.github.com>
2022-10-21 16:51:06 +0200

    Review uninstall

--- uninstall.bat
+++ uninstall.bat
@@ -1,2 +1,5 @@
 @echo off
-powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" -Uninstall'" -Verb RunAs
+
+SET arguments=-Uninstall
+
+powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" %arguments%'" -Verb RunAs

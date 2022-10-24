230d3eec619f427e35ec07685c6fd10bac7dc7d4
Romain <96626929+Romanitho@users.noreply.github.com>
2022-10-25 01:51:03 +0200

    added InstallUserContext argument

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
 
-SET arguments=-UpdatesAtLogon -UpdatesInterval Weekly -StartMenuShortcut
+SET arguments=-UpdatesAtLogon -UpdatesInterval Weekly -StartMenuShortcut -InstallUserContext
 
 powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-AutoUpdate-Install.ps1"" %arguments%'" -Verb RunAs

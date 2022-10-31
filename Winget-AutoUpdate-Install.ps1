fdd6171eeaa9058af48a796e4fe781b950fde1c7
romanitho <96626929+Romanitho@users.noreply.github.com>
2022-10-31 17:25:37 +0100

    😵🔫

--- Winget-AutoUpdate-Install.ps1
+++ Winget-AutoUpdate-Install.ps1
@@ -255,9 +255,9 @@ function Install-WingetAutoUpdate {
         # Set regkeys for notification name, icon and actions
         & reg add "HKCR\AppUserModelId\Windows.SystemToast.Winget.Notification" /v DisplayName /t REG_EXPAND_SZ /d "Application Update" /f | Out-Null
         & reg add "HKCR\AppUserModelId\Windows.SystemToast.Winget.Notification" /v IconUri /t REG_EXPAND_SZ /d %SystemRoot%\system32\@WindowsUpdateToastIcon.png /f | Out-Null
-        $WAUClass = "HKLM:\Software\Classes\WAU\shell\open\command"
+        $WAUClass = "HKLM:\Software\Classes\WAU"
         $WAUClassRun = "Wscript.exe ""$WingetUpdatePath\Invisible.vbs"" ""C:\Windows\System32\schtasks.exe /run /tn Winget-AutoUpdate"""
-        New-Item $WAUClass -Force -ErrorAction SilentlyContinue | Out-Null
+        New-Item "$WAUClass\shell\open\command" -Force -ErrorAction SilentlyContinue | Out-Null
         New-ItemProperty -LiteralPath $WAUClass -Name 'URL Protocol' -Value '' -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
         New-ItemProperty -LiteralPath $WAUClass -Name '(default)' -Value "URL:$($ActionType)" -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
         New-ItemProperty -LiteralPath $WAUClass -Name 'EditFlags' -Value '2162688' -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null

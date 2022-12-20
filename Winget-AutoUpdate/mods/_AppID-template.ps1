7a9864d3e999c6a253cc6878e0974c77761fe4f4
KnifMelti <gaj@knifmelti.se>
2022-12-20 03:19:46 +0100

    More info

--- Winget-AutoUpdate/mods/_AppID-template.ps1
+++ Winget-AutoUpdate/mods/_AppID-template.ps1
@@ -2,10 +2,10 @@
 #Beginning of Process Name to Stop - optional wildcard (*) after, without .exe, multiple: "proc1","proc2"
 $Proc = @("")
 
-#Beginning of Process Name to Wait for to end - optional wildcard (*) after, without .exe, multiple: "proc1","proc2"
+#Beginning of Process Name to Wait for to End - optional wildcard (*) after, without .exe, multiple: "proc1","proc2"
 $Wait = @("")
 
-#Beginning of App Name string to Uninstall - required wildcard (*) after!
+#Beginning of App Name string to Silently Uninstall (MSI/NSIS/INNO) - required wildcard (*) after!
 $App = ""
 
 #Beginning of Desktop Link Name to Remove - optional wildcard (*) after, without .lnk, multiple: "lnk1","lnk2"

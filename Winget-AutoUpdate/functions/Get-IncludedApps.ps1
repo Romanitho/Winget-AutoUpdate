021170b72bfe8962c464a337a3807fcc08eb88f3
KnifMelti <gaj@knifmelti.se>
2023-01-21 17:37:03 +0100

    Small bits

--- Winget-AutoUpdate/functions/Get-IncludedApps.ps1
+++ Winget-AutoUpdate/functions/Get-IncludedApps.ps1
@@ -6,17 +6,19 @@ function Get-IncludedApps {
 
         $Key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\WhiteList\'
         $ValueNames = (Get-Item -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\WhiteList").Property
-        
-        foreach ($ValueName in $ValueNames) {
-            $AppIDs = [Microsoft.Win32.Registry]::GetValue($Key, $ValueName, $false)
-            [PSCustomObject]@{
-                Value = $ValueName
-                Data = $AppIDs.Trim()
+
+        if ($ValueNames) {
+            foreach ($ValueName in $ValueNames) {
+                $AppIDs = [Microsoft.Win32.Registry]::GetValue($Key, $ValueName, $false)
+                [PSCustomObject]@{
+                    Value = $ValueName
+                    Data = $AppIDs.Trim()
+                }
             }
+    
+            return $AppIDs
         }
-
-        return $AppIDs
-
+        
     }
     elseif (Test-Path "$WorkingDir\included_apps.txt") {
 

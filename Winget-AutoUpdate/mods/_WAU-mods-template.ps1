d28a686df5542fdc3c870c0e4cb22de1b2c78770
KnifMelti <gaj@knifmelti.se>
2023-09-04 01:11:52 +0200

    Block comment is better

--- Winget-AutoUpdate/mods/_WAU-mods-template.ps1
+++ Winget-AutoUpdate/mods/_WAU-mods-template.ps1
@@ -1,9 +1,10 @@
-#Mods for WAU (if Network is active/any Winget is installed/running as SYSTEM)
-#Winget-Upgrade.ps1 calls this script with the code:
-#[Write-ToLog "Running Mods for WAU..." "Yellow"
-#& "$Mods\_WAU-mods.ps1"]
-#Make sure your Functions have unique names!
-#Exit 1 to Re-run WAU from this script!
+<# #Mods for WAU (if Network is active/any Winget is installed/running as SYSTEM)
+Winget-Upgrade.ps1 calls this script with the code:
+[Write-ToLog "Running Mods for WAU..." "Yellow"
+& "$Mods\_WAU-mods.ps1"]
+Make sure your Functions have unique names!
+Exit 1 to Re-run WAU from this script!
+#>
 
 <# FUNCTIONS #>
 

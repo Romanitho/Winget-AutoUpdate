46fb0b5cb70ddffdd7e9c675a22340b3b3b3e585
KnifMelti <gaj@knifmelti.se>
2023-09-04 01:00:27 +0200

    small text

--- Winget-AutoUpdate/mods/_WAU-mods-template.ps1
+++ Winget-AutoUpdate/mods/_WAU-mods-template.ps1
@@ -2,6 +2,7 @@
 #Winget-Upgrade.ps1 calls this script with the code:
 #[Write-ToLog "Running Mods for WAU..." "Yellow"
 #& "$Mods\_WAU-mods.ps1"]
+#Make sure your Functions have unique names!
 #Exit 1 to Re-run WAU from this script!
 
 <# FUNCTIONS #>

fba9ccf111eb05b9336c1a1bf3d2cb228c5da65e
Romain <96626929+Romanitho@users.noreply.github.com>
2023-09-21 18:12:11 +0200

    Fix runas if bat not run as admin

--- uninstall.bat
+++ uninstall.bat
@@ -11,5 +11,5 @@ SET arguments=-Uninstall
 :: Run Powershell Script  ::
 ::::::::::::::::::::::::::::
 
-SET PowershellCmd=Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file "%~dp0Winget-AutoUpdate-Install.ps1" %arguments%
-powershell -Command "& {Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; %PowershellCmd%'}" -Verb RunAs
+SET PowershellCmd=Start-Process powershell.exe -Verb RunAs -Argument '-noprofile -executionpolicy bypass -file "%~dp0Winget-AutoUpdate-Install.ps1" %arguments%
+powershell -Command "& {Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; %PowershellCmd%'}"

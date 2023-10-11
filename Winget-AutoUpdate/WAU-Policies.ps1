575292c16b94873b04677308542b95b55c552201
romanitho <96626929+Romanitho@users.noreply.github.com>
2023-10-12 00:53:21 +0200

    Testing and adjustments

--- Winget-AutoUpdate/WAU-Policies.ps1
+++ Winget-AutoUpdate/WAU-Policies.ps1
@@ -16,28 +16,45 @@ $WAUConfig = Get-WAUConfig
 #Check if GPO got applied from Get-WAUConfig
 if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
 
+    #Log init
+    $GPOLogFile = "$($WAUConfig.InstallLocation)\logs\LatestAppliedSettings.txt"
+    Set-Content -Path $GPOLogFile -Value "###  POLICY CYCLE - $(Get-Date)  ###`n"
+
+    #Reset WAU_RunGPOManagement if not GPO managed anymore (This is used to run this job one last time and reset initial settings)
+    if ($($WAUConfig.WAU_ActivateGPOManagement -eq 1)) {
+        Add-Content -Path $GPOLogFile -Value "GPO Management Enabled."
+    }
+    else {
+        New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Winget-AutoUpdate" -Name WAU_RunGPOManagement -Value 0 -Force | Out-Null
+        $WAUConfig.WAU_RunGPOManagement = 0
+        Add-Content -Path $GPOLogFile -Value "GPO Management Disabled. Policies removed."
+    }
+
     #Update 'Winget-AutoUpdate' scheduled task settings
     $taskTriggers = @()
     if ($WAUConfig.WAU_UpdatesAtLogon -eq 1) {
         $tasktriggers += New-ScheduledTaskTrigger -AtLogOn
     }
     if ($WAUConfig.WAU_UpdatesInterval -eq "Daily") {
-        $tasktriggers += New-ScheduledTaskTrigger -Daily -At $UpdatesAtTime
+        $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime
     }
     elseif ($WAUConfig.WAU_UpdatesInterval -eq "BiDaily") {
-        $tasktriggers += New-ScheduledTaskTrigger -Daily -At $UpdatesAtTime -DaysInterval 2
+        $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime -DaysInterval 2
     }
     elseif ($WAUConfig.WAU_UpdatesInterval -eq "Weekly") {
-        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $UpdatesAtTime -DaysOfWeek 2
+        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2
     }
     elseif ($WAUConfig.WAU_UpdatesInterval -eq "BiWeekly") {
-        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 2
+        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 2
     }
     elseif ($WAUConfig.WAU_UpdatesInterval -eq "Monthly") {
-        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 4
+        $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 4
     }
     if ($taskTriggers) {
-        Get-ScheduledTask -TaskName 'Winget-AutoUpdate' -ErrorAction SilentlyContinue | Set-ScheduledTask -Trigger $taskTriggers
+        #Get Winget-AutoUpdate scheduled task
+        $WAUTask = Get-ScheduledTask -TaskName 'Winget-AutoUpdate' -ErrorAction SilentlyContinue
+        #Edit scheduled task
+        Set-ScheduledTask -TaskPath $WAUTask.TaskPath -TaskName $WAUTask.TaskName -Trigger $taskTriggers | Out-Null
     }
 
     #Update Desktop shortcut
@@ -46,7 +63,7 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
         Add-Shortcut "wscript.exe" $DesktopShortcut "`"$($WAUConfig.InstallLocation)\Invisible.vbs`" `"powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"`"`"$($WAUConfig.InstallLocation)\user-run.ps1`"`"" "${env:SystemRoot}\System32\shell32.dll,-16739" "Manual start of Winget-AutoUpdate (WAU)..."
     }
     elseif ($WAUConfig.WAU_DesktopShortcut -ne 1) {
-        Remove-Item -Path $DesktopShortcut -Force | Out-Null
+        Remove-Item -Path $DesktopShortcut -Force -ErrorAction SilentlyContinue | Out-Null
     }
 
     #Update Start Menu shortcuts
@@ -58,22 +75,12 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
         Add-Shortcut "wscript.exe" "$StartMenuShortcut\WAU - Web Help.lnk" "`"$($WAUConfig.InstallLocation)\Invisible.vbs`" `"powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"`"`"$($WAUConfig.InstallLocation)\user-run.ps1`" -Help`"" "${env:SystemRoot}\System32\shell32.dll,-24" "Help for WAU..."
     }
     elseif ($WAUConfig.WAU_StartMenuShortcut -ne 1) {
-        Remove-Item -Path $StartMenuShortcut -Recurse -Force | Out-Null
+        Remove-Item -Path $StartMenuShortcut -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
     }
 
     #Log latest applied config
-    $GPOLogFile = "$($WAUConfig.InstallLocation)\logs\LatestAppliedSettings.txt"
-    Set-Content -Path $GPOLogFile -Value "### POLICY CYCLE - $(Get-Date) ###"
-    $WAUConfig.PSObject.Properties | Where-Object { $_.Name -like "WAU_*" } | Select-Object Name, Value | Out-File -FilePath $GPOLogFile -Append
-
-    #Reset WAU_RunGPOManagement if not GPO managed anymore (This is used to run this job one last time and reset initial settings)
-    if ($($WAUPolicies.WAU_ActivateGPOManagement -eq 1)) {
-        Add-Content -Path $GPOLogFile -Value "GPO Management Enabled."
-    }
-    else {
-        New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Winget-AutoUpdate" -Name WAU_RunGPOManagement -Value 0 -Force | Out-Null
-        Add-Content -Path $GPOLogFile -Value "GPO Management Disabled. Policies removed."
-    }
+    Add-Content -Path $GPOLogFile -Value "`nLatest applied settings:"
+    $WAUConfig.PSObject.Properties | Where-Object { $_.Name -like "WAU_*" } | Select-Object Name, Value | Out-File -Encoding default -FilePath $GPOLogFile -Append
 }
 
 Exit 0

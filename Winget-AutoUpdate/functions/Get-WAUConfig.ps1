efbaa6d3e51a3f2be04f39155d3f1068df3c5be0
romanitho <96626929+Romanitho@users.noreply.github.com>
2023-10-12 14:00:10 +0200

    remove write in "get" function

--- Winget-AutoUpdate/functions/Get-WAUConfig.ps1
+++ Winget-AutoUpdate/functions/Get-WAUConfig.ps1
@@ -2,28 +2,23 @@
 
 Function Get-WAUConfig {
 
-    #Check if GPO Management is enabled
-    $ActivateGPOManagement = Get-ItemPropertyValue "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -Name "WAU_ActivateGPOManagement" -ErrorAction SilentlyContinue
-
-    if ($ActivateGPOManagement -eq 1) {
-
-        #Add a tag to activate WAU-Policies scheduled task
-        New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Winget-AutoUpdate" -Name WAU_RunGPOManagement -Value 1 -Force | Out-Null
-
-        #Get all WAU Policies
-        $WAUPolicies = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -ErrorAction SilentlyContinue
-    }
-
     #Get WAU Configurations from install config
     $WAUConfig = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Winget-AutoUpdate" -ErrorAction SilentlyContinue
 
+    #Check if GPO Management is enabled
+    $ActivateGPOManagement = Get-ItemPropertyValue "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -Name "WAU_ActivateGPOManagement" -ErrorAction SilentlyContinue
+
     #If GPO Management is enabled, replace settings
     if ($ActivateGPOManagement -eq 1) {
+
+        #Get all WAU Policies
+        $WAUPolicies = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -ErrorAction SilentlyContinue
 
         #Replace loaded configurations by ones from Policies
         $WAUPolicies.PSObject.Properties | ForEach-Object {
             $WAUConfig.PSObject.Properties.add($_)
         }
+
     }
 
     #Return config

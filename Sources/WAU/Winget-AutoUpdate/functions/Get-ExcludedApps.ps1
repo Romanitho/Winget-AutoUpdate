a8bfbe6ab964c01628a2ee31fe79fe2de2d65275
Nick Brown <109540830+nickjisc@users.noreply.github.com>
2024-03-15 09:39:51 +0000

    Added the uri fething from #586

--- Sources/WAU/Winget-AutoUpdate/functions/Get-ExcludedApps.ps1
+++ Sources/WAU/Winget-AutoUpdate/functions/Get-ExcludedApps.ps1
@@ -2,28 +2,49 @@
 
 function Get-ExcludedApps {
 
+    $AppIDs = @()
+
+    #region blacklist in registry
     if ($GPOList) {
 
         if (Test-Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\BlackList") {
-
-            $Key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\BlackList\'
-
             $ValueNames = (Get-Item -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\BlackList").Property
-
-            $AppIDs = @()
 
             foreach ($ValueName in $ValueNames) {
                 $AppIDs += (Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\BlackList" -Name $ValueName).Trim()
             }
 
         }
-        return $AppIDs | Where-Object { $_.length -gt 0 }
+
+    }     
+    #endregion blacklist in registry
+    #region blacklist pulled from URI
+    elseif ($URIList) {
+
+        $RegPath = "$WAU_GPORoot";
+        $RegValueName = 'WAU_URIList';
+        
+        if (Test-Path -Path $RegPath) {
+            $RegKey = Get-Item -Path $RegPath;
+            $WAUURI = $RegKey.GetValue($RegValueName);
+            if ($null -ne $WAUURI) {
+                $resp = Invoke-WebRequest -Uri $WAUURI -UseDefaultCredentials;
+                if ($resp.BaseResponse.StatusCode -eq [System.Net.HttpStatusCode]::OK) {
+                    $resp.Content.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) | 
+                    ForEach-Object {
+                        $AppIds += $_
+                    }
+                }
+            }
+        }
 
     }
+    #endregion blacklist pulled from URI
     elseif (Test-Path "$WorkingDir\excluded_apps.txt") {
 
         return (Get-Content -Path "$WorkingDir\excluded_apps.txt").Trim() | Where-Object { $_.length -gt 0 }
 
     }
 
+    return $AppIDs | Where-Object { $_.length -gt 0 }
 }

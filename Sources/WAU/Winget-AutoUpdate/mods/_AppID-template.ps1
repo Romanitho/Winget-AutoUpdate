ea0113744a2d8084e7679fc73314a8895e8ac02d
Philipp Rüegsegger <pr@nexplore.ch>
2024-03-19 07:51:16 +0100

    fix: indents

--- Sources/WAU/Winget-AutoUpdate/mods/_AppID-template.ps1
+++ Sources/WAU/Winget-AutoUpdate/mods/_AppID-template.ps1
@@ -87,52 +87,52 @@ $User = $True
 
 <# MAIN #>
 if ($RunSystem) {
-  Invoke-ModsApp $RunSystem $RunSwitch $RunWait ""
+    Invoke-ModsApp $RunSystem $RunSwitch $RunWait ""
 }
 if ($Proc) {
-  Stop-ModsProc $Proc
+    Stop-ModsProc $Proc
 }
 if ($Svc) {
   Stop-ModsSvc $Svc
 }
 if ($Wait) {
-  Wait-ModsProc $Wait
+    Wait-ModsProc $Wait
 }
 if ($WingetIDInst) {
-  Install-WingetID $WingetIDInst
+    Install-WingetID $WingetIDInst
 }
 if ($WingetIDUninst) {
-  Uninstall-WingetID $WingetIDUninst
+    Uninstall-WingetID $WingetIDUninst
 }
 if ($AppUninst) {
-  Uninstall-ModsApp $AppUninst $AllVersions
+    Uninstall-ModsApp $AppUninst $AllVersions
 }
 if ($Lnk) {
-  Remove-ModsLnk $Lnk
+    Remove-ModsLnk $Lnk
 }
 if ($AddKey -and $AddValue -and $AddTypeData -and $AddType) {
-  Add-ModsReg $AddKey $AddValue $AddTypeData $AddType
+    Add-ModsReg $AddKey $AddValue $AddTypeData $AddType
 }
 if ($DelKey) {
-  Remove-ModsReg $DelKey $DelValue
+    Remove-ModsReg $DelKey $DelValue
 }
 if ($DelFile) {
-  Remove-ModsFile $DelFile
+    Remove-ModsFile $DelFile
 }
 if ($RenFile -and $NewName) {
-  Rename-ModsFile $RenFile $NewName
+    Rename-ModsFile $RenFile $NewName
 }
 if ($CopyFile -and $CopyTo) {
-  Copy-ModsFile $CopyFile $CopyTo
+    Copy-ModsFile $CopyFile $CopyTo
 }
 if ($File -and $FindText -and $ReplaceText) {
-  Edit-ModsFile $File $FindText $ReplaceText
+    Edit-ModsFile $File $FindText $ReplaceText
 }
 if ($GrantPath) {
-  Grant-ModsPath $GrantPath
+    Grant-ModsPath $GrantPath
 }
 if ($RunUser) {
-  Invoke-ModsApp $RunUser "" "" $User
+    Invoke-ModsApp $RunUser "" "" $User
 }
 
 <# EXTRAS #>

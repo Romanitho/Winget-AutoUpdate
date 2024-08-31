1bd4b1300a46719065573e1d25d12a2a2a719865
Romain <96626929+Romanitho@users.noreply.github.com>
2024-08-31 15:22:02 +0200

    [MSI Prerequisites] Check for msi as well

--- Sources/WAU/Winget-AutoUpdate/functions/Update-WAU.ps1
+++ Sources/WAU/Winget-AutoUpdate/functions/Update-WAU.ps1
@@ -13,6 +13,7 @@ function Update-WAU {
 
     #Run WAU update
     try {
+        #Try WAU.zip (v1)
 
         #Force to create a zip file
         $ZipFile = "$WorkingDir\WAU_update.zip"
@@ -68,13 +69,39 @@ function Update-WAU {
 
     catch {
 
-        #Send Error Notif
-        $Title = $NotifLocale.local.outputs.output[4].title -f "Winget-AutoUpdate"
-        $Message = $NotifLocale.local.outputs.output[4].message
-        $MessageType = "error"
-        Start-NotifTask -Title $Title -Message $Message -MessageType $MessageType -Button1Action $OnClickAction -Button1Text $Button1Text
-        Write-ToLog "WAU Update failed" "Red"
+        #Try WAU.msi (v2)
+        try {
+            #Download the msi
+            Write-ToLog "Downloading the GitHub Repository version $WAUAvailableVersion" "Cyan"
+            $MsiFile = "$env:temp\WAU.msi"
+            Invoke-RestMethod -Uri "https://github.com/Romanitho/Winget-AutoUpdate/releases/download/v$($WAUAvailableVersion)/WAU.msi" -OutFile $MsiFile
 
+
+
+            #Update WAU and run
+            Write-ToLog "Updating WAU..." "Yellow"
+            Start-Process msiexec.exe -ArgumentList "/i $MsiFile /qn /L*v ""$WorkingDir\logs\WAU-Installer.log"" RUN_WAU=YES" -Wait
+
+            #Send success Notif
+            Write-ToLog "WAU Update completed. Rerunning WAU..." "Green"
+            $Title = $NotifLocale.local.outputs.output[3].title -f "Winget-AutoUpdate"
+            $Message = $NotifLocale.local.outputs.output[3].message -f $WAUAvailableVersion
+            $MessageType = "success"
+            Start-NotifTask -Title $Title -Message $Message -MessageType $MessageType -Button1Action $OnClickAction -Button1Text $Button1Text
+
+            Exit 0
+        }
+
+        catch {
+
+            #Send Error Notif
+            $Title = $NotifLocale.local.outputs.output[4].title -f "Winget-AutoUpdate"
+            $Message = $NotifLocale.local.outputs.output[4].message
+            $MessageType = "error"
+            Start-NotifTask -Title $Title -Message $Message -MessageType $MessageType -Button1Action $OnClickAction -Button1Text $Button1Text
+            Write-ToLog "WAU Update failed" "Red"
+
+        }
     }
 
 }

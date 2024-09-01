8379e72f868790dac8a5d17bbc5bcf8d484a5911
Romain <96626929+Romanitho@users.noreply.github.com>
2024-09-01 15:54:36 +0200

    clean

--- Sources/Winget-AutoUpdate/functions/Get-IncludedApps.ps1
+++ Sources/Winget-AutoUpdate/functions/Get-IncludedApps.ps1
@@ -3,45 +3,47 @@
 function Get-IncludedApps {
     $AppIDs = @()
 
-    #region whitelist in registry
+    #whitelist in registry
     if ($GPOList) {
 
+        Write-ToLog "-> Included apps from GPO is activated"
         if (Test-Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\WhiteList") {
             $ValueNames = (Get-Item -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\WhiteList").Property
-
             foreach ($ValueName in $ValueNames) {
                 $AppIDs += (Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate\WhiteList" -Name $ValueName).Trim()
             }
-
+            Write-ToLog "-> Successsfully loaded included apps list."
         }
 
-    }     
-    #endregion whitelist in registry
-    #region whitelist pulled from URI
+    }
+    #whitelist pulled from URI
     elseif ($URIList) {
 
         $RegPath = "$WAU_GPORoot";
         $RegValueName = 'WAU_URIList';
-        
+
         if (Test-Path -Path $RegPath) {
             $RegKey = Get-Item -Path $RegPath;
             $WAUURI = $RegKey.GetValue($RegValueName);
+            Write-ToLog "-> Included apps from URI is activated"
             if ($null -ne $WAUURI) {
                 $resp = Invoke-WebRequest -Uri $WAUURI -UseDefaultCredentials;
                 if ($resp.BaseResponse.StatusCode -eq [System.Net.HttpStatusCode]::OK) {
-                    $resp.Content.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) | 
+                    $resp.Content.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) |
                     ForEach-Object {
                         $AppIds += $_
                     }
+                    Write-ToLog "-> Successsfully loaded included apps list."
                 }
             }
         }
 
     }
-    #endregion whitelist pulled from URI
+    #whitelist pulled from local file
     elseif (Test-Path "$WorkingDir\included_apps.txt") {
 
-        return (Get-Content -Path "$WorkingDir\included_apps.txt").Trim() | Where-Object { $_.length -gt 0 }
+        $AppIDs = (Get-Content -Path "$WorkingDir\included_apps.txt").Trim()
+        Write-ToLog "-> Successsfully loaded local included apps list."
 
     }
 

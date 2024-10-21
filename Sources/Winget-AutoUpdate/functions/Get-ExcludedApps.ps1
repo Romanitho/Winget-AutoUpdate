ef1fec71a509d3490e9942e05a5e77906e222b22
Fabian Seitz <fabian.seitz@pari.com>
2024-10-21 17:51:17 +0200

    Exclude pinned winget Apps from WAU
    As requested in #644

--- Sources/Winget-AutoUpdate/functions/Get-ExcludedApps.ps1
+++ Sources/Winget-AutoUpdate/functions/Get-ExcludedApps.ps1
@@ -55,5 +55,47 @@ function Get-ExcludedApps {
 
     }
 
-    return $AppIDs | Where-Object { $_.length -gt 0 }
+    #blacklist pinned winget apps
+    $pinnedAppsResult = & $Winget pin list | Where-Object { $_ -notlike "   *" } | Out-String
+    if (!($pinnedAppsResult -match "-----")) {
+        Write-ToLog "-> No pinned winget apps found, nothing to exclude."
+    } else {
+        # Split winget output to lines
+        $lines = $pinnedAppsResult.Split([Environment]::NewLine) | Where-Object { $_ }
+
+        # Find the line that starts with "------"
+        $fl = 0
+        while (-not $lines[$fl].StartsWith("-----")) {
+            $fl++
+        }
+
+        # Get header line
+        $fl = $fl - 1
+
+        # Get header titles and calculate start positions of each column
+        $index = $lines[$fl] -split '\s{2,}'
+        $idStart = $lines[$fl].IndexOf($index[1])
+        $versionStart = $lines[$fl].IndexOf($index[2])
+
+        # Now cycle through the real package lines and split accordingly
+        For ($i = $fl + 2; $i -lt $lines.Length; $i++) {
+            $line = $lines[$i] -replace "[\u2026]", " " # Fix "..." in long names
+            if (-Not ($line.StartsWith("-----"))) {
+
+                # (Alphanumeric | Literal . | Alphanumeric) - the only unique thing in common for lines with applications
+                if ($line -match "\w\.\w") {
+                    $softwareId = $line.Substring($idStart, $versionStart - $idStart).TrimEnd()
+                    if ($null -ne $softwareId) {
+                        # Add the extracted software ID to the list
+                        $AppIds += $softwareId
+                    }
+                }
+            }
+
+        # Output the result for debugging
+        Write-Host "Extracted App IDs: $($AppIds -join ', ')"
+        }
+
+        return $AppIDs | Where-Object { $_.length -gt 0 }
+    }
 }

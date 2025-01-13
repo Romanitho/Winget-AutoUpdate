431839acb7da1990316d068bde22f8061c616551
Mickaël CHAVE <38375531+mchave3@users.noreply.github.com>
2025-01-13 15:48:48 +0100

    Improved Logging function :
    - Changed $LogMsg to a mandatory parameter.
    - Optimized ACL settings for the log file.
    - Added try/catch block for error handling.
    - Modified the "Echo log" behavior to improve clarity.
    - Added UTF-8 encoding for better character support.

--- Sources/Winget-AutoUpdate/functions/Write-ToLog.ps1
+++ Sources/Winget-AutoUpdate/functions/Write-ToLog.ps1
@@ -4,38 +4,39 @@ function Write-ToLog {
 
     [CmdletBinding()]
     param(
-        [Parameter()] [String] $LogMsg,
+        [Parameter(Mandatory = $true)] [String] $LogMsg,
         [Parameter()] [String] $LogColor = "White",
         [Parameter()] [Switch] $IsHeader = $false
     )
 
-    #Create file if doesn't exist
-    if (!(Test-Path $LogFile)) {
-        New-Item -ItemType File -Path $LogFile -Force | Out-Null
+    try {
+        #Create file if doesn't exist
+        if (!(Test-Path $LogFile)) {
+            New-Item -ItemType File -Path $LogFile -Force | Out-Null
 
-        #Set ACL for users on logfile
-        $NewAcl = Get-Acl -Path $LogFile
-        $identity = New-Object System.Security.Principal.SecurityIdentifier S-1-5-11
-        $fileSystemRights = "Modify"
-        $type = "Allow"
-        $fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
-        $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
-        $NewAcl.SetAccessRule($fileSystemAccessRule)
-        Set-Acl -Path $LogFile -AclObject $NewAcl
+            #Set ACL for users on logfile
+            $NewAcl = Get-Acl -Path $LogFile
+            $identity = New-Object System.Security.Principal.SecurityIdentifier S-1-5-11
+            $fileSystemAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "Modify", "Allow")
+            $NewAcl.SetAccessRule($fileSystemAccessRule)
+            Set-Acl -Path $LogFile -AclObject $NewAcl
+        }
+
+        #If header requested
+        if ($IsHeader) {
+            $Log = "#" * 65 + "`n#    $(Get-Date -Format (Get-culture).DateTimeFormat.ShortDatePattern) - $LogMsg`n" + "#" * 65
+        }
+        else {
+            $Log = "$(Get-Date -UFormat "%T") - $LogMsg"
+        }
+
+        #Echo log
+        Write-Host $Log -ForegroundColor $LogColor
+
+        #Write log to file
+        $Log | Out-File -FilePath $LogFile -Append -Encoding utf8
     }
-
-    #If header requested
-    if ($IsHeader) {
-        $Log = "#" * 65 + "`n#    $(Get-Date -Format (Get-culture).DateTimeFormat.ShortDatePattern) - $LogMsg`n" + "#" * 65
+    catch {
+        Write-Error "Error writing to log file: $($_.Exception.Message)"
     }
-    else {
-        $Log = "$(Get-Date -UFormat "%T") - $LogMsg"
-    }
-
-    #Echo log
-    $Log | Write-host -ForegroundColor $LogColor
-
-    #Write log to file
-    $Log | Out-File -FilePath $LogFile -Append
-
 }

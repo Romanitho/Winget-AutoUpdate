8d6a158041f40cc92f8fbc20e78c33a62c8507b3
Andrzej Demski <75534654+AndrewDemski-ad-gmail-com@users.noreply.github.com>
2025-01-26 18:27:49 +0100

    Update Get-WingetCmd.ps1
    ..I need vacation

--- Sources/Winget-AutoUpdate/functions/Get-WingetCmd.ps1
+++ Sources/Winget-AutoUpdate/functions/Get-WingetCmd.ps1
@@ -4,14 +4,14 @@ Function Get-WingetCmd {
     [OutputType([String])]
 
     $WingetCmd = [string]::Empty;
-    try {
     #Get WinGet Path
     # default winget path (in system context)
     [string]$ps = "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_8wekyb3d8bbwe\winget.exe";
 
     #default winget path (in user context)
     [string]$pu = "$env:LocalAppData\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe";
-
+    
+    try {
         #Get Admin Context Winget Location
         $WingetInfo = (Get-Item -Path $ps -ErrorAction Stop).VersionInfo | Sort-Object -Property FileVersionRaw -Descending | Select-Object -First 1;
         #If multiple versions, pick most recent one

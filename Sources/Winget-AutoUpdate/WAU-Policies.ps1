996b2d4b9c6fff7cc3a0dd93130a7e477cc7948f
cyronis <p.traenker@kelvindynamics.com>
2025-07-28 11:13:56 +0200

    updated to current version - inplace copy of the pin logic

--- Sources/Winget-AutoUpdate/WAU-Policies.ps1
+++ Sources/Winget-AutoUpdate/WAU-Policies.ps1
@@ -9,18 +9,13 @@ Daily update settings from policies
 #Import functions
 . "$PSScriptRoot\functions\Get-WAUConfig.ps1"
 
-#Check if GPO Management is enabled
-$ActivateGPOManagement = Get-ItemPropertyValue "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -Name "WAU_ActivateGPOManagement" -ErrorAction SilentlyContinue
-if ($ActivateGPOManagement -eq 1) {
-    #Add (or update) tag to activate WAU-Policies Management
-    New-ItemProperty "HKLM:\SOFTWARE\Romanitho\Winget-AutoUpdate" -Name WAU_RunGPOManagement -Value 1 -Force | Out-Null
-}
+#Check if GPO Management is detected
+$GPOManagementDetected = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -ErrorAction SilentlyContinue
 
-#Get WAU settings
-$WAUConfig = Get-WAUConfig
+if ($GPOManagementDetected) {
 
-#Check if GPO got applied from Get-WAUConfig (tag)
-if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
+    #Get WAU settings
+    $WAUConfig = Get-WAUConfig
 
     #Log init
     $GPOLogDirectory = Join-Path -Path $WAUConfig.InstallLocation -ChildPath "logs"
@@ -29,16 +24,6 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
     }
     $GPOLogFile = Join-Path -Path $GPOLogDirectory -ChildPath "LatestAppliedSettings.txt"
     Set-Content -Path $GPOLogFile -Value "###  POLICY CYCLE - $(Get-Date)  ###`n"
-
-    #Reset WAU_RunGPOManagement if not GPO managed anymore (This is used to run this job one last time and reset initial settings)
-    if ($($WAUConfig.WAU_ActivateGPOManagement -eq 1)) {
-        Add-Content -Path $GPOLogFile -Value "GPO Management Enabled. Policies updated."
-    }
-    else {
-        New-ItemProperty "HKLM:\SOFTWARE\Romanitho\Winget-AutoUpdate" -Name WAU_RunGPOManagement -Value 0 -Force | Out-Null
-        $WAUConfig.WAU_RunGPOManagement = 0
-        Add-Content -Path $GPOLogFile -Value "GPO Management Disabled. Policies removed."
-    }
 
     #Get Winget-AutoUpdate scheduled task
     $WAUTask = Get-ScheduledTask -TaskName 'Winget-AutoUpdate' -ErrorAction SilentlyContinue
@@ -49,7 +34,7 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
 
     #Check if LogOn trigger setting has changed
     $hasLogonTrigger = $currentTriggers | Where-Object { $_.CimClass.CimClassName -eq "MSFT_TaskLogonTrigger" }
-    if (($WAUConfig.WAU_UpdatesAtLogon -eq 1 -and -not $hasLogonTrigger) -or 
+    if (($WAUConfig.WAU_UpdatesAtLogon -eq 1 -and -not $hasLogonTrigger) -or
         ($WAUConfig.WAU_UpdatesAtLogon -ne 1 -and $hasLogonTrigger)) {
         $configChanged = $true
     }
@@ -87,9 +72,20 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
         $configChanged = $true
     }
 
+    #Check if delay has changed
+    $randomDelay = [TimeSpan]::ParseExact($WAUConfig.WAU_UpdatesTimeDelay, "hh\:mm", $null)
+    $timeTrigger = $currentTriggers | Where-Object { $_.CimClass.CimClassName -ne "MSFT_TaskLogonTrigger" } | Select-Object -First 1
+    if ($timeTrigger.RandomDelay -match '^PT(?:(\d+)H)?(?:(\d+)M)?$') {
+        $hours = if ($matches[1]) { [int]$matches[1] } else { 0 }
+        $minutes = if ($matches[2]) { [int]$matches[2] } else { 0 }
+        $existingRandomDelay = New-TimeSpan -Hours $hours -Minutes $minutes
+    }
+    if ($existingRandomDelay -ne $randomDelay) {
+        $configChanged = $true
+    }
+
     #Check if schedule time has changed
     if ($currentIntervalType -ne "None" -and $currentIntervalType -ne "Never") {
-        $timeTrigger = $currentTriggers | Where-Object { $_.CimClass.CimClassName -ne "MSFT_TaskLogonTrigger" } | Select-Object -First 1
         if ($timeTrigger) {
             $currentTime = [DateTime]::Parse($timeTrigger.StartBoundary).ToString("HH:mm:ss")
             if ($currentTime -ne $WAUConfig.WAU_UpdatesAtTime) {
@@ -105,21 +101,21 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
             $tasktriggers += New-ScheduledTaskTrigger -AtLogOn
         }
         if ($WAUConfig.WAU_UpdatesInterval -eq "Daily") {
-            $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime
+            $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime -RandomDelay $randomDelay
         }
         elseif ($WAUConfig.WAU_UpdatesInterval -eq "BiDaily") {
-            $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime -DaysInterval 2
+            $tasktriggers += New-ScheduledTaskTrigger -Daily -At $WAUConfig.WAU_UpdatesAtTime -DaysInterval 2 -RandomDelay $randomDelay
         }
         elseif ($WAUConfig.WAU_UpdatesInterval -eq "Weekly") {
-            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2
+            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -RandomDelay $randomDelay
         }
         elseif ($WAUConfig.WAU_UpdatesInterval -eq "BiWeekly") {
-            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 2
+            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 2 -RandomDelay $randomDelay
         }
         elseif ($WAUConfig.WAU_UpdatesInterval -eq "Monthly") {
-            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 4
+            $tasktriggers += New-ScheduledTaskTrigger -Weekly -At $WAUConfig.WAU_UpdatesAtTime -DaysOfWeek 2 -WeeksInterval 4 -RandomDelay $randomDelay
         }
-        
+
         #If trigger(s) set
         if ($taskTriggers) {
             #Edit scheduled task
@@ -132,10 +128,11 @@ if ($WAUConfig.WAU_RunGPOManagement -eq 1) {
             Set-ScheduledTask -TaskPath $WAUTask.TaskPath -TaskName $WAUTask.TaskName -Trigger $tasktriggers | Out-Null
         }
     }
-    
+
     #Log latest applied config
     Add-Content -Path $GPOLogFile -Value "`nLatest applied settings:"
     $WAUConfig.PSObject.Properties | Where-Object { $_.Name -like "WAU_*" } | Select-Object Name, Value | Out-File -Encoding default -FilePath $GPOLogFile -Append
+
 }
 
 Exit 0
--- Sources/Winget-AutoUpdate/functions/Get-WAUConfig.ps1
+++ Sources/Winget-AutoUpdate/functions/Get-WAUConfig.ps1
@@ -6,20 +6,17 @@ Function Get-WAUConfig {
     $WAUConfig_64_86 = Get-ItemProperty -Path "HKLM:\SOFTWARE\Romanitho\Winget-AutoUpdate*", "HKLM:\SOFTWARE\WOW6432Node\Romanitho\Winget-AutoUpdate*" -ErrorAction SilentlyContinue | Sort-Object { $_.ProductVersion } -Descending
     $WAUConfig = $WAUConfig_64_86[0]
 
-    #Check if GPO Management is enabled
-    $ActivateGPOManagement = Get-ItemPropertyValue "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -Name "WAU_ActivateGPOManagement" -ErrorAction SilentlyContinue
+    #Check if GPO policies exist
+    $WAUPolicies = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -ErrorAction SilentlyContinue
 
-    #If GPO Management is enabled, replace settings
-    if ($ActivateGPOManagement -eq 1) {
-
-        #Get all WAU Policies
-        $WAUPolicies = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Romanitho\Winget-AutoUpdate" -ErrorAction SilentlyContinue
+    #If GPO policies exist, apply them (regardless of ActivateGPOManagement value)
+    if ($WAUPolicies) {
+        Write-ToLog "GPO policies detected - applying GPO configuration" "Yellow"
 
         #Replace loaded configurations by ones from Policies
         $WAUPolicies.PSObject.Properties | ForEach-Object {
             $WAUConfig.PSObject.Properties.add($_)
         }
-
     }
 
     #Return config
